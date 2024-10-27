41d909c129aa8a1d09b1653db85a500100373ba3
leigiel <nasifabdullahp@gmail.com>
2024-10-27 22:13:12 +0600

    final

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -9,7 +9,7 @@ const n9 = document.getElementById("num9");
 const prdct = document.getElementById("product");
 const n4 = document.getElementById("num4");
 const n5 = document.getElementById("num5");
-const n66 = document.getElementById("num6");
+const n6 = document.getElementById("num6");
 const mns = document.getElementById("minus");
 const n1 = document.getElementById("num1");
 const n2 = document.getElementById("num2");
@@ -20,6 +20,65 @@ const flt = document.getElementById("float");
 const n0 = document.getElementById("num0");
 const eql = document.getElementById("equal");
 
+let first = null;
+let operator = null;
+let second = null;
+
+function clickNum(num) {
+  drslt.innerText += num;
+}
+
+
+n1.addEventListener("click", () => clickNum('1'));
+n2.addEventListener("click", () => clickNum('2'));
+n3.addEventListener("click", () => clickNum('3'));
+n4.addEventListener("click", () => clickNum('4'));
+n5.addEventListener("click", () => clickNum('5'));
+n6.addEventListener("click", () => clickNum('6'));
+n7.addEventListener("click", () => clickNum('7'));
+n8.addEventListener("click", () => clickNum('8'));
+n9.addEventListener("click", () => clickNum('9'));
+n0.addEventListener("click", () => clickNum('0'));
+flt.addEventListener("click", () => clickNum('.'));
+
+
+function clickOp(op) {
+  first = parseFloat(drslt.innerText);
+  operator = op; 
+  drslt.innerText = "";
+}
+
+pls.addEventListener("click", () => clickOp('+'));
+mns.addEventListener("click", () => clickOp('-'));
+prdct.addEventListener("click", () => clickOp('*'));
+dvd.addEventListener("click", () => clickOp('/'));
+
 eql.addEventListener("click",function(){
-  console.log("");
-});
+    second = parseFloat(drslt.innerText);
+  
+    let result;
+    if (operator === '+') {
+      result = first + second;
+    } else if (operator === '-') {
+      result = first - second;
+    } else if (operator === '*') {
+      result = first * second;
+    } else if (operator === '/') {
+      result = first / second;
+    }
+  
+    drslt.innerText = result;
+    first = result;
+    operator = null;
+    second = null;
+  })
+  allclr.addEventListener("click",function(){
+    drslt.innerText = "";
+    operator = null;
+    second = null;
+  });
+
+  dlt.addEventListener("click",function(){
+    drslt.innerText = drslt.innerText.slice(0, -1);
+  });
+  
